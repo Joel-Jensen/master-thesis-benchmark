c47924edf0a130a7aceadf8e950bdd3aa7bed40e
Joel-Jensen <60270137+Joel-Jensen@users.noreply.github.com>
2025-05-18 10:27:03 +0200

    Init

---
+++ benchmark.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eux
+
+./run.sh 2>&1 | tee log.txt
+
+sudo du -bcs /var/lib/postgresql/$PGVERSION/main/
+
+cat log.txt | grep -oP 'Time: \d+\.\d+ ms' | sed -r -e 's/Time: ([0-9]+\.[0-9]+) ms/\1/' |
+    awk '{ if (i % 3 == 0) { printf "[" }; printf $1 / 1000; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
---
+++ load.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eux
+
+threads=$(nproc)
+cpus=$(($threads / 2))
+
+# Using COPY with explicit column mapping to ensure correct alignment.
+time split transactions_1M.csv --verbose -n r/$cpus --filter='sudo -u postgres psql test -t -c "\\copy transactions (id, user_id, amount, type, country_code, platform, created_at) FROM STDIN"'
+
+#time sudo -u postgres psql test -t <index.sql
+
+time sudo -u postgres psql test -t -c 'VACUUM ANALYZE transactions'

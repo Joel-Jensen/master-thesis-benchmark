de39b9f43c2cfda2547e735c75fe9dd4db0a7f01
Joel-Jensen <60270137+Joel-Jensen@users.noreply.github.com>
2025-05-18 15:32:19 +0200

    fix: store temporary as files

--- load.sh
+++ load.sh
@@ -6,8 +6,12 @@ threads=$(nproc)
 cpus=$((threads > 1 ? threads / 2 : 1))
 
 # Using COPY with explicit column mapping to ensure correct alignment.
-time split transactions_1M.csv --verbose -n r/$cpus --filter=`sudo -u postgres psql test -t -c "\\copy transactions (id, user_id, amount, type, country_code, platform, created_at) FROM STDIN DELIMITER ',' CSV HEADER"`
-
+time split transactions_1M.csv split/transactions_chunk_
+for chunk in split/transactions_chunk_*
+do
+    echo "Loading $chunk into database..."
+    sudo -u postgres psql test -c "\\copy transactions (id, user_id, amount, type, country_code, platform, created_at) FROM '$chunk' DELIMITER ',' CSV HEADER"
+done
 #time sudo -u postgres psql test -t <index.sql
 
 time sudo -u postgres psql test -t -c 'VACUUM ANALYZE transactions'

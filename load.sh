a74bace696abd4350efbb6b0a698cb2c4c361f65
Joel-Jensen <60270137+Joel-Jensen@users.noreply.github.com>
2025-05-18 10:53:35 +0200

    add create script

--- load.sh
+++ load.sh
@@ -6,7 +6,7 @@ threads=$(nproc)
 cpus=$(($threads / 2))
 
 # Using COPY with explicit column mapping to ensure correct alignment.
-time split transactions_1M.csv --verbose -n r/$cpus --filter='sudo -u postgres psql test -t -c "\\copy transactions (id, user_id, amount, type, country_code, platform, created_at) FROM STDIN"'
+time split transactions_1M.csv --verbose -n r/$cpus --filter='sudo -u postgres psql test -t -c "\\copy transactions (id, user_id, amount, type, country_code, platform, created_at) FROM STDIN DELIMITER ',' CSV HEADER"'
 
 #time sudo -u postgres psql test -t <index.sql
 

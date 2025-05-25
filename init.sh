0a61b06af456da50f01f5a0ea97d23969441540f
Joel-Jensen <60270137+Joel-Jensen@users.noreply.github.com>
2025-05-25 19:23:53 +0200

    Add download for users

--- init.sh
+++ init.sh
@@ -11,3 +11,6 @@ gzip -d -f transactions_10M.csv.gz
 sed -i '1d' transactions_10M.csv
 mkdir -p split_10M
 time split -l 100000 transactions_10M.csv split_10M/transactions_chunk_
+
+wget --no-verbose --continue 'https://inly-master-thesis.ams3.digitaloceanspaces.com/users_10M.csv.gz'
+gzip -d -f users_10M.csv.gz

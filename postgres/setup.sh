c5f1cd5fdb5c7c97943c5682312dd9ae0217ee96
Joel-Jensen <60270137+Joel-Jensen@users.noreply.github.com>
2025-05-26 18:35:18 +0200

    Use 1M

--- postgres/setup.sh
+++ postgres/setup.sh
@@ -18,7 +18,7 @@ threads=$(nproc)
 cpus=$((threads > 1 ? threads / 2 : 1))
 
 # Using COPY with explicit column mapping to ensure correct alignment.
-for chunk in ../split_10M/transactions_chunk_*
+for chunk in ../split_1M/transactions_chunk_*
 do
     #echo "Loading $chunk into database..."
     sudo -u postgres psql test -c "\\copy transactions (id, user_id, amount, type, country_code, platform, created_at) FROM '$chunk' DELIMITER ','"

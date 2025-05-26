615fcf656fc85d4b807377f369b78019a9e59a96
Joel-Jensen <60270137+Joel-Jensen@users.noreply.github.com>
2025-05-26 18:38:37 +0200

    fix cols

--- postgres/setup.sh
+++ postgres/setup.sh
@@ -23,7 +23,7 @@ do
     #echo "Loading $chunk into database..."
     sudo -u postgres psql test -c "\\copy transactions (id, user_id, amount, type, country_code, platform, created_at) FROM '$chunk' DELIMITER ','"
 done
-sudo -u postgres psql test -c "\\copy users (id, name, email, email_verified_at, password, country_code, is_active, remember_token) FROM '../users_10M.csv' DELIMITER ','"
+sudo -u postgres psql test -c "\\copy users (id, name, email, email_verified_at, password, country_code, is_active, remember_token, created_at, updated_at) FROM '../users_10M.csv' DELIMITER ','"
 #time sudo -u postgres psql test -t <index.sql
 
 time sudo -u postgres psql test -t -c 'VACUUM ANALYZE transactions'

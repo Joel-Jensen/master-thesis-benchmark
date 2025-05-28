169c456b08c7bccb92a000fc305a8237c66fcfd7
Joel-Jensen <60270137+Joel-Jensen@users.noreply.github.com>
2025-05-28 19:50:58 +0200

    revert file changes

--- pg_mooncake/install.sh
+++ pg_mooncake/install.sh
@@ -2,13 +2,8 @@
 
 set -eux
 
-# Get the absolute path of the parent directory
-PARENT_DIR="$(cd .. && pwd)"
 
-docker run -d --name pg_mooncake -p 5433:5432 -e POSTGRES_HOST_AUTH_METHOD=trust \
-    -v "${PARENT_DIR}/split_10M:/tmp/split_10M" \
-    -v "${PARENT_DIR}/users_10M.csv:/tmp/users_10M.csv" \
-    mooncakelabs/pg_mooncake:17-v0.1.0
+docker run -d --name pg_mooncake -p 5433:5432 -e POSTGRES_HOST_AUTH_METHOD=trust mooncakelabs/pg_mooncake:17-v0.1.0
 
 sleep 5
 psql postgres://postgres:pg_mooncake@localhost:5433/postgres -f create.sql
--- pg_mooncake/setup.sh
+++ pg_mooncake/setup.sh
@@ -10,12 +10,12 @@ threads=$(nproc)
 cpus=$((threads > 1 ? threads / 2 : 1))
 
 # Using COPY with explicit column mapping to ensure correct alignment.
-for chunk in /tmp/split_10M/transactions_chunk_*
+for chunk in ../split_10M/transactions_chunk_*
 do
     #echo "Loading $chunk into database..."
     psql postgres://postgres:pg_mooncake@localhost:5433/postgres -c "\\copy transactions (id, user_id, amount, type, country_code, platform, created_at) FROM '$chunk' DELIMITER ','"
 done
-psql postgres://postgres:pg_mooncake@localhost:5433/postgres -c "\\copy users (id, name, email, email_verified_at, password, country_code, is_active, remember_token, created_at, updated_at) FROM '/tmp/users_10M.csv' DELIMITER ','"
+psql postgres://postgres:pg_mooncake@localhost:5433/postgres -c "\\copy users (id, name, email, email_verified_at, password, country_code, is_active, remember_token, created_at, updated_at) FROM '../users_10M.csv' DELIMITER ','"
 #time psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t <index.sql
 
 time psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c 'VACUUM ANALYZE transactions'

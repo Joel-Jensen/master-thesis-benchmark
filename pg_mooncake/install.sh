35f36e8aa70090a34378c6833733d7ad44c95299
Joel-Jensen <60270137+Joel-Jensen@users.noreply.github.com>
2025-05-28 18:06:56 +0200

    Fix file linkage

--- pg_mooncake/install.sh
+++ pg_mooncake/install.sh
@@ -2,7 +2,10 @@
 
 set -eux
 
-docker run -d --name pg_mooncake -p 5433:5432 -e POSTGRES_HOST_AUTH_METHOD=trust -v ./hits.parquet:/tmp/hits.parquet mooncakelabs/pg_mooncake:17-v0.1.0
+docker run -d --name pg_mooncake -p 5433:5432 -e POSTGRES_HOST_AUTH_METHOD=trust \
+    -v ../split_10M:/tmp/split_10M \
+    -v ../users_10M.csv:/tmp/users_10M.csv \
+    mooncakelabs/pg_mooncake:17-v0.1.0
 
 sleep 5
 psql postgres://postgres:pg_mooncake@localhost:5433/postgres -f create.sql

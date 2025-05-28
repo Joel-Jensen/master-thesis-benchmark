12b801024635dfd7dfb046d9a4a32b9913604a38
Joel-Jensen <60270137+Joel-Jensen@users.noreply.github.com>
2025-05-28 22:43:06 +0200

    Use latest version

--- pg_mooncake/install.sh
+++ pg_mooncake/install.sh
@@ -10,7 +10,7 @@ docker run -d --name pg_mooncake -p 5433:5432 -e POSTGRES_HOST_AUTH_METHOD=trust
     -v "${PARENT_DIR}/users_10M.csv:/tmp/users_10M.csv" \
     -v "${PARENT_DIR}/transactions-10m-2.parquet:/tmp/transactions-10m-2.parquet" \
     -v "${PARENT_DIR}/users-10m.parquet:/tmp/users-10m.parquet" \
-    mooncakelabs/pg_mooncake:17-v0.1.0
+    mooncakelabs/pg_mooncake
 
 sleep 5
 psql postgres://postgres:pg_mooncake@localhost:5433/postgres -f create.sql

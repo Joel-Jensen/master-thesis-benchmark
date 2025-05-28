8b6b8da250d87b2bb9152ab208529ef35df28674
Joel-Jensen <60270137+Joel-Jensen@users.noreply.github.com>
2025-05-28 22:42:48 +0200

    Insert to normal table

--- pg_mooncake/setup.sh
+++ pg_mooncake/setup.sh
@@ -10,7 +10,7 @@ threads=$(nproc)
 cpus=$((threads > 1 ? threads / 2 : 1))
 
 psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c "INSERT INTO transactions SELECT id, user_id, amount, type, country_code, platform, 'epoch'::timestamp + (created_at || 'second')::interval created_at FROM mooncake.read_parquet('/tmp/transactions-10m-2.parquet') as(id BIGINT, user_id BIGINT, amount INT, type VARCHAR, country_code VARCHAR, platform VARCHAR, created_at BIGINT)"
-psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c "INSERT INTO users SELECT id, name, email, 'epoch'::timestamp + (email_verified_at || 'second')::interval email_verified_at, password, country_code, is_active, remember_token, 'epoch'::timestamp + (created_at || 'second')::interval created_at, 'epoch'::timestamp + (updated_at || 'second')::interval updated_at FROM mooncake.read_parquet('/tmp/users-10m.parquet') as (id BIGINT, name VARCHAR, email VARCHAR, email_verified_at BIGINT, password VARCHAR, country_code VARCHAR, is_active BOOLEAN, remember_token VARCHAR, created_at BIGINT, updated_at BIGINT)"
+psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c "\\copy users (id, name, email, email_verified_at, password, country_code, is_active, remember_token, created_at, updated_at) FROM '../users_10M.csv' DELIMITER ','"
 #time psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t <index.sql
 
 time psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c 'VACUUM ANALYZE transactions'

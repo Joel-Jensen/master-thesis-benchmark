debe4689e47b5daeecfc98f99e749d92987de4f1
Joel-Jensen <60270137+Joel-Jensen@users.noreply.github.com>
2025-06-01 14:41:00 +0200

    Import from csv

--- pg_mooncake/setup.sh
+++ pg_mooncake/setup.sh
@@ -9,7 +9,7 @@ psql postgres://postgres:pg_mooncake@localhost:5433/postgres test -t < create.sq
 threads=$(nproc)
 cpus=$((threads > 1 ? threads / 2 : 1))
 
-psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c "INSERT INTO transactions SELECT id, user_id, amount, type, country_code, platform, 'epoch'::timestamp + (created_at || 'second')::interval created_at FROM mooncake.read_csv('/tmp/transactions-10M.csv') as(id BIGINT, user_id BIGINT, amount INT, type VARCHAR, country_code VARCHAR, platform VARCHAR, created_at BIGINT)"
+psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c "INSERT INTO transactions SELECT id, user_id, amount, type, country_code, platform, created_at::timestamp AS created_at FROM mooncake.read_csv('/tmp/transactions_10M.csv') as (id BIGINT, user_id BIGINT, amount INT, type VARCHAR, country_code VARCHAR, platform VARCHAR, created_at TEXT)"
 psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c "\\copy users (id, name, email, email_verified_at, password, country_code, is_active, remember_token, created_at, updated_at) FROM '../users_10M.csv' DELIMITER ','"
 #time psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t <index.sql
 

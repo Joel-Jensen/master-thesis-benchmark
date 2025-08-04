9be89fe3995f82f5ca0d3ec2dafffee1a626eb53
Joel-Jensen <60270137+Joel-Jensen@users.noreply.github.com>
2025-08-04 22:06:16 +0200

    fix

--- pg_mooncake/install.sh
+++ pg_mooncake/install.sh
@@ -17,7 +17,6 @@ docker run -d --name pg_mooncake -p 5433:5432 -e POSTGRES_HOST_AUTH_METHOD=trust
 
 sleep 5
 
-psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t < create.sql
 
 # COPY 99997497
 # Time: 576219.151 ms (09:36.219)
--- pg_mooncake/setup.sh
+++ pg_mooncake/setup.sh
@@ -2,18 +2,16 @@
 
 set -eux
 
-psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c 'DROP TABLE transactions'
-psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c 'DROP TABLE users'
-psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t < create.sql
-
 threads=$(nproc)
 cpus=$((threads > 1 ? threads / 2 : 1))
+
+psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t < create.sql
 
 psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c "INSERT INTO transactions SELECT id, user_id, amount, type, country_code, platform, created_at::timestamp AS created_at FROM mooncake.read_csv('/tmp/transactions_10M.csv') as (id BIGINT, user_id BIGINT, amount INT, type VARCHAR, country_code VARCHAR, platform VARCHAR, created_at TEXT)"
 psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c "\\copy users (id, name, email, email_verified_at, password, country_code, is_active, remember_token, created_at, updated_at) FROM '../users_10M.csv' DELIMITER ','"
 #time psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t <index.sql
 
 # This is like VACUUM ANALYZE but for pg_mooncake
-time psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c 'UPDATE transactions SET id=id'
+# time psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c 'UPDATE transactions SET id=id'
 
 ./benchmark.sh

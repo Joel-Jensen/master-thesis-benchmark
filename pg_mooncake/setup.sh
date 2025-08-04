11881a398ac768fc6115726d5f58c304673baf2c
Joel-Jensen <60270137+Joel-Jensen@users.noreply.github.com>
2025-08-04 22:10:23 +0200

    test

--- pg_mooncake/setup.sh
+++ pg_mooncake/setup.sh
@@ -7,7 +7,11 @@ cpus=$((threads > 1 ? threads / 2 : 1))
 
 psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t < create.sql
 
-psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c "INSERT INTO transactions SELECT r['id']::BIGINT, r['user_id']::BIGINT, r['amount']::INT, r['type'], r['country_code'], r['platform'], r['created_at']::TIMESTAMP FROM mooncake.read_csv('/tmp/transactions_10M.csv', AUTO_DETECT=TRUE) r"
+for chunk in ../split_10M/transactions_chunk_*
+do
+    #echo "Loading $chunk into database..."
+    psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c "\\copy transactions (id, user_id, amount, type, country_code, platform, created_at) FROM '$chunk' DELIMITER ','"
+done
 psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c "\\copy users (id, name, email, email_verified_at, password, country_code, is_active, remember_token, created_at, updated_at) FROM '../users_10M.csv' DELIMITER ','"
 #time psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t <index.sql
 

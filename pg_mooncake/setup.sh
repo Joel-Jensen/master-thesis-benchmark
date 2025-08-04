308e03fd33e3cd7916347c446c6e609be8e2666f
Joel-Jensen <60270137+Joel-Jensen@users.noreply.github.com>
2025-08-04 22:09:04 +0200

    test

--- pg_mooncake/setup.sh
+++ pg_mooncake/setup.sh
@@ -7,7 +7,7 @@ cpus=$((threads > 1 ? threads / 2 : 1))
 
 psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t < create.sql
 
-psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c "INSERT INTO transactions SELECT id, user_id, amount, type, country_code, platform, created_at::timestamp AS created_at FROM mooncake.read_csv('/tmp/transactions_10M.csv') as (id BIGINT, user_id BIGINT, amount INT, type VARCHAR, country_code VARCHAR, platform VARCHAR, created_at TEXT)"
+psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c "INSERT INTO transactions SELECT r['id']::BIGINT, r['user_id']::BIGINT, r['amount']::INT, r['type'], r['country_code'], r['platform'], r['created_at']::TIMESTAMP FROM mooncake.read_csv('/tmp/transactions_10M.csv', AUTO_DETECT=TRUE) r"
 psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t -c "\\copy users (id, name, email, email_verified_at, password, country_code, is_active, remember_token, created_at, updated_at) FROM '../users_10M.csv' DELIMITER ','"
 #time psql postgres://postgres:pg_mooncake@localhost:5433/postgres -t <index.sql
 
